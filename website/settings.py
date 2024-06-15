330af48520c24e1e4586264c254e58cd975b3f57
brandon23567 <brandonmahuvava0@gmail.com>
2024-06-15 18:37:30 +0530

    added and connected to new db

--- website/settings.py
+++ website/settings.py
@@ -74,10 +74,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres.snzlzqylpsfdjicigpay',
+        'USER': 'postgres.qifldbhxqaejhadeariw',
         'PASSWORD': 'StrongestAvenger#23',
         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
-        'PORT': '5432',
+        'PORT': '6543',
     }
 }
 
